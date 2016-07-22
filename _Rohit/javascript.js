5085f7be8d78d14a6ff57452c8c7db59a859d6a5
Divyanshu <divyanshu.1996@gmail.com>
2016-07-22 17:55:23 +0530

    fixed bugs

--- _Rohit/javascript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.menu-parent').on('click', function(){
-        $(this).toggleClass('active');
-        $('.pane').toggleClass('pane-display');
-        $('.sidebar').toggleClass('sidebar-toggle');
-        $('.section-wrapper').toggleClass('section-position')
-        })
-});
